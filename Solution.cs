0f9b33764a604dca57d1776d3743ee89af3b7e01
Lachezar Kutsarov <lkutsarov@gmail.com>
2025-06-29 13:13:06 +0200

    Create Solution.cs

---
+++ Solution.cs
@@ -0,0 +1,47 @@
+
+using System;
+
+public class Solution
+{
+    private static readonly int ALPHABET_SIZE = 26;
+    private static readonly string[] DIGIT_TO_WORD =
+            {
+              "zero", "one", "two", "three", "four", 
+              "five", "six", "seven", "eight", "nine"
+            };
+
+    public int CountOddLetters(int input)
+    {
+        int[] frequency = createFrequency(input);
+
+        int numberOfOddLetters = 0;
+        foreach (int current in frequency)
+        {
+            if (isOdd(current))
+            {
+                ++numberOfOddLetters;
+            }
+        }
+        return numberOfOddLetters;
+    }
+
+    private int[] createFrequency(int input)
+    {
+        int[] frequency = new int[ALPHABET_SIZE];
+        while (input > 0)
+        {
+            int digit = input % 10;
+            for (int i = 0; i < DIGIT_TO_WORD[digit].Length; ++i)
+            {
+                ++frequency[DIGIT_TO_WORD[digit][i] - 'a'];
+            }
+            input /= 10;
+        }
+        return frequency;
+    }
+
+    private bool isOdd(int value)
+    {
+        return value % 2 == 1;
+    }
+}
